7d499a4b5ff7bb734c791cbe00bc730804bf6b01
jcanell4 <jcanell4@xtec.cat>
2026-02-18 10:17:28 +0100

    czmbio versión de pyspark

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='py_portada_data_layer',
-      version='0.1.27',
+      version='0.1.29',
       description='Data layer for portada project for ETL process',
       author='PortADa team',
       author_email='jcbportada@gmail.com',
@@ -10,7 +10,7 @@ setup(name='py_portada_data_layer',
       packages=['portada_data_layer'],
       py_modules=['delta_data_layer', 'data_lake_metadata_manager','traced_data_frame', "portada_ingestion", "portada_cleaning"],
       install_requires=[
-        'pyspark==3.5.3',
+        'pyspark==3.5.8',
         'delta-spark==3.2.1',
         'hdfs==2.7.0',
         'py4j',
